17568193ba6993777d4ad8270f95abb143efbd9e
hcd <hcd@126.com>
2017-05-24 00:02:35 +0800

    对应关系

--- application/admin/controller/Relation.php
+++ application/admin/controller/Relation.php
@@ -13,7 +13,15 @@
 		 */
 		public function index(){
 			$res=Db::name('category')->where('status',1)->where('parentid','<>',0)->order('paixu,catid')->select();
+			$zdlist=array();
+			//字段
+			foreach ($res as $k => $v) {
+				$catid=$v['catid'];
+				$catarr=Db::name('attr_category')->alias('a')->join('hcd_category b','a.catid=b.catid')->join('hcd_attr c','a.attrid=c.id')->where('a.catid',$catid)->select();
+				$zdlist[$catid]=$catarr;
+			}
 			$this->assign('lists',$res);
+			$this->assign('zdlist',$zdlist);
 			return $this->fetch();
 		}
 	}
