c6f7a102811347409e7d97337d53ebe6c96d784e
hcd <hcdong2008@126.com>
2017-05-24 18:50:09 +0800

    产品管理

--- application/admin/controller/Product.php
+++ application/admin/controller/Product.php
@@ -5,12 +5,19 @@
 
 	class Product extends AdminBase{
 		/**
-		 * 商品分类
+		 * 产品列表
 		 * @Author   黄传东
-		 * @DateTime 2017-05-17T15:52:18+0800
+		 * @DateTime 2017-05-24T18:21:08+0800
 		 * @return   [type]                   [description]
 		 */
-		public function category(){
+		public function index(){
+			$res=Db::name('product')->order('id desc')->paginate(20);
+			$catarr=Db::name('category')->select();
+			foreach($catarr as $k=>$v){
+				$catlist[$v['catid']]=$v['catname'];
+			}
+			$this->assign('catlist',$catlist);
+			$this->assign('lists',$res);
 			return $this->fetch();
 		}
 	}
--- application/admin/controller/Relation.php
+++ application/admin/controller/Relation.php
@@ -24,5 +24,22 @@
 			$this->assign('zdlist',$zdlist);
 			return $this->fetch();
 		}
+		/**
+		 * 分类属性详情
+		 * @Author   黄传东
+		 * @DateTime 2017-05-24T17:18:49+0800
+		 * @return   [type]                   [description]
+		 */
+		public function detail(){
+			$param=$this->param;
+			$catid=$param['catid'];
+			$attrid=$param['attrid'];
+			$res=Db::name('attr_detail')->where('attrid',$attrid)->where('catid',$catid)->select();
+			$str='';
+			foreach ($res as $k => $v) {
+				$str.="<option value='".$v['id']."'>".$v['title']."</option>";
+			}
+			return $str;
+		}
 	}
 ?>
